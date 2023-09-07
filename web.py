b7d59fe5325f463dc27d4b6eb1891054f30f0fb7
chashtag <matthew.cundari@gmail.com>
2023-09-07 10:35:09 -0400

    update

--- web.py
+++ web.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return open('/dev/mouse_miles').read()
+    r_list = []
+    remotes = request.args.get('remote','')
+    if remotes:
+        for remote in remotes.split(','):
+            r_list.append(remote.split(':'))
+        
+    return render_template('index.html',remotes=r_list)
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
